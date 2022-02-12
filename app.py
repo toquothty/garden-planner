cbc089f9fe1fd0b33ab1624de181149d52f19683
Thomas Toquothty <tltoquothty@gmail.com>
2022-02-11 20:06:47 -0500

    Doing initial database layout and creation

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db = SQLAlchemy(app)
+
+
+class Garden_DB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vegetable = db.Column(db.String(200), nullable=False)
+    sow_timeline = db.Column(db.String(200))  # When to sow (direct, indoors)
+    sow_type = db.Column(db.String)  # Direct, Indoors
+    harvest_days = db.Column(db.Integer)  # How many days to harvest
+    plant_spacing = db.Column(db.Integer)  # Inches between plants
+    seed_depth = db.Column(db.Integer)  # Inches for seed depth
+    sow_window = db.Column(db.Date)  # Window to sow, regardless of sow type
+    transplant_window = db.Column(db.Date)  # Window to transplant if applicable
+    harvest_window = db.Column(db.Date)  # Window to harvest
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
