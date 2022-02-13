59febfb0509bbb79f09ebf935289ae5c5620e0fc
Thomas Toquothty <tltoquothty@gmail.com>
2022-02-13 14:09:11 -0500

    Flushed out /update, added new DB columns into table. Having issues with datetime linking betwene the html form and the DB

--- app.py
+++ app.py
@@ -11,15 +11,19 @@ db = SQLAlchemy(app)
 class Garden_DB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     vegetable = db.Column(db.String(200), nullable=False)
-    sow_timeline = db.Column(db.String(200))  # When to sow (direct, indoors)
-    sow_type = db.Column(db.String)  # Direct, Indoors
+    sow_timeline = db.Column(db.String(200))  # When to sow (direct, indoors) data
+    sow_type = db.Column(db.String)  # Direct, Indoors, key used to filter later
     harvest_days = db.Column(db.Integer)  # How many days to harvest
     plant_spacing = db.Column(db.Integer)  # Inches between plants
     seed_depth = db.Column(db.Integer)  # Inches for seed depth
-    sow_window = db.Column(db.Date)  # Window to sow, regardless of sow type
-    transplant_window = db.Column(db.Date)  # Window to transplant if applicable
-    harvest_window = db.Column(db.Date)  # Window to harvest
+    sow_window_start = db.Column(db.String)  # Window to sow, regardless of sow type
+    sow_window_end = db.Column(db.String)  # Window to sow, regardless of sow type
+    transplant_window_start = db.Column(db.String)  # Window to transplant if applicable
+    transplant_window_end = db.Column(db.String)  # Window to transplant if applicable
+    harvest_window_start = db.Column(db.String)  # Window to harvest
+    harvest_window_end = db.Column(db.String)  # Window to harvest
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    # db.create_all
 
 
 @app.route("/", methods=["GET"])
@@ -46,14 +50,38 @@ def display_vegetables():
 def update_vegetable():
     if request.method == "POST":
         vegetable_creation = request.form["vegetable"].title()
-        new_vegetable = Garden_DB(vegetable=vegetable_creation)
+        sow_timeline_creation = request.form["sow_timeline"]
+        sow_type_creation = request.form["sow_type"].title()
+        harvest_days_creation = request.form["harvest_days"]
+        plant_spacing_creation = request.form["plant_spacing"]
+        seed_depth_creation = request.form["seed_depth"]
+        sow_window_begin = request.form["sow_window_start"]
+        sow_window_close = request.form["sow_window_end"]
+        transplant_window_begin = request.form["transplant_window_start"]
+        transplant_window_close = request.form["transplant_window_end"]
+        harvest_window_begin = request.form["harvest_window_start"]
+        harvest_window_close = request.form["harvest_window_end"]
+        new_vegetable = Garden_DB(
+            vegetable=vegetable_creation,
+            sow_timeline=sow_timeline_creation,
+            sow_type=sow_type_creation,
+            harvest_days=harvest_days_creation,
+            plant_spacing=plant_spacing_creation,
+            seed_depth=seed_depth_creation,
+            sow_window_start=sow_window_begin,
+            sow_window_end=sow_window_close,
+            transplant_window_start=transplant_window_begin,
+            transplant_window_end=transplant_window_close,
+            harvest_window_start=harvest_window_begin,
+            harvest_window_end=harvest_window_close,
+        )
 
         try:
             db.session.add(new_vegetable)
             db.session.commit()
             return redirect("/all")
-        except:
-            return "There was an issue adding your task"
+        except Exception as ex:
+            return ex
     else:
         return render_template("update.html")
 
